00c3600b4e0c4b680057d779353ef8d7c396ce7c
Vishudh-cell <vishudh.24bcs10162@sst.scaler.com>
2026-03-02 12:35:44 +0530

    immutable tickets done

---
+++ immutable-tickets/immutable-tickets/src/com/example/tickets/TicketService.java
@@ -0,0 +1,23 @@
+package com.example.tickets;
+
+public class TicketService {
+
+    public IncidentTicket createTicket(String id, String reporterEmail, String title) {
+        return IncidentTicket.builder(id, reporterEmail, title)
+                .addTag("NEW")
+                .build();
+    }
+
+    public IncidentTicket escalateToCritical(IncidentTicket t) {
+        return t.toBuilder()
+                .priority("CRITICAL")
+                .addTag("ESCALATED")
+                .build();
+    }
+
+    public IncidentTicket assign(IncidentTicket t, String assigneeEmail) {
+        return t.toBuilder()
+                .assigneeEmail(assigneeEmail)
+                .build();
+    }
+}
